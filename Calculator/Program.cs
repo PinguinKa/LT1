90cec019ed35fc40a78cba59b0196d9a3fb32d3e
PinguinK <pinguink@inbox.ru>
2023-09-27 14:16:49 +0300

    Changes to be committed:

---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double num1, num2;
+            char op;
+            Console.WriteLine("First number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Choose (+, -, *, /): ");
+            op = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case '-':
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case '*':
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case '/':
+                    if (num2 == 0) Console.WriteLine("Division by zero");
+                    else Console.WriteLine(num1 / num2);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}

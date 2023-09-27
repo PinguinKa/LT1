9b6691a063a78f0254394b0499918e7e8884d874
PinguinK <pinguink@inbox.ru>
2023-09-27 13:54:12 +0300

    calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double num1, num2;
+            char op;
+            Console.WriteLine("First number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Choose (+, -, *, /): ");
+            op = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case '-':
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case '*':
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case '/':
+                    Console.WriteLine(num1 / num2);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
